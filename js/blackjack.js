118b53195e99d029a915092b261edb97f726931d
mselbekk11 <mselbekk11@gmail.com>
2023-10-09 12:23:53 -0700

    got the logic working as well as added a deal (restart) button

--- js/blackjack.js
+++ js/blackjack.js
@@ -56,8 +56,87 @@ function startGame() {
   hidden = deck.pop() // Moves a card from the end of the array
   dealerSum += getValue(hidden)
   dealerAceCount += checkAce(hidden)
-  console.log(hidden);
-  console.log(dealerSum);
+  // console.log(hidden);
+  // console.log(dealerSum);
+  while (dealerSum < 17) {
+    // <img src="./cards/4-C.png">
+    let cardImg = document.createElement('img')
+    let card = deck.pop()
+    cardImg.src = './cards/' + card + '.png'
+    dealerSum += getValue(card)
+    dealerAceCount += checkAce(card)
+    document.getElementById('dealer-cards').append(cardImg)
+  }
+  console.log(dealerSum)
+
+  for (let i = 0; i < 2; i++) {
+    let cardImg = document.createElement('img')
+    let card = deck.pop()
+    cardImg.src = './cards/' + card + '.png'
+    yourSum += getValue(card)
+    yourAceCount += checkAce(card)
+    document.getElementById('your-cards').append(cardImg)
+  }
+
+  console.log(yourSum)
+  // document.getElementById('deal').addEventListener('click', deal)
+  document.getElementById('hit').addEventListener('click', hit)
+  document.getElementById('stay').addEventListener('click', stay)
+}
+
+function hit() {
+  if (!canHit) {
+    return
+  }
+
+  let cardImg = document.createElement('img')
+  let card = deck.pop()
+  cardImg.src = './cards/' + card + '.png'
+  yourSum += getValue(card)
+  yourAceCount += checkAce(card)
+  document.getElementById('your-cards').append(cardImg)
+
+  if (reduceAce(yourSum, yourAceCount) > 21) {
+    canHit = false
+  }
+}
+
+function stay() {
+  dealerSum = reduceAce(dealerSum, dealerAceCount)
+  yourSum = reduceAce(yourSum, yourAceCount)
+
+  canHit = false
+  document.getElementById('hidden').src = './cards/' + hidden + '.png'
+
+  let message = ''
+  if (yourSum > 21) {
+    message = 'You Lose!'
+  } else if (dealerSum > 21) {
+    message = 'You Win!'
+  }
+  // both you and dealer <= 21
+  else if (yourSum == dealerSum) {
+    message = 'Its a Tie!'
+  } else if (yourSum > dealerSum) {
+    message = 'You Win!'
+  } else if (yourSum < dealerSum) {
+    message = 'You Lose!'
+  }
+
+  // const btn = document.createElement('button')
+  // document.getElementById('buttons').append(btn)
+  // btn.innerText = 'Deal'
+  // btn.classList.add('deal')
+
+  document.getElementById('dealer-sum').innerText = dealerSum
+  document.getElementById('your-sum').innerText = yourSum
+  document.getElementById('results').innerText = message
+  document.getElementById('buttons').innerHTML = ''
+  document.getElementById(
+    'buttons'
+  ).innerHTML = `<button id="deal" type="button" onClick="window.location.reload()">
+  Deal
+</button>`
 }
 
 function getValue(card) {
@@ -80,4 +159,13 @@ function checkAce(card) {
     return 1
   }
   return 0
+}
+
+// turns your Aces into 1s if you are exceeding 21
+function reduceAce(playerSum, playerAceCount) {
+  while (playerSum > 21 && playerAceCount > 0) {
+    playerSum -= 10
+    playerAceCount -= 1
+  }
+  return playerSum
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log('this is a test')
+// ...
+
+// Add an event listener to the restart button
+document.getElementById('restart').addEventListener('click', restartGame);
+
+function restartGame() {
+  // Reset all game-related variables
+  dealerSum = 0;
+  yourSum = 0;
+  dealerAceCount = 0;
+  yourAceCount = 0;
+  canHit = true;
+
+  // Clear the dealer and your card displays
+  document.getElementById('dealer-cards').innerHTML = '';
+  document.getElementById('your-cards').innerHTML = '';
+
+  // Clear the hidden card and result messages
+  document.getElementById('hidden').src = '';
+  document.getElementById('results').innerText = '';
+
+  // Rebuild and shuffle the deck
+  buildDeck();
+  shuffleDeck();
+
+  // Start a new game
+  startGame();
+}
+
+// ...
